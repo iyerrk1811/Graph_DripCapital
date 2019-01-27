b4b95b8b1689bd0ba260a6eb8cdd8f7d72006183
iyerrk1811 <30715370+iyerrk1811@users.noreply.github.com>
2019-01-27 14:16:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/graph-dc'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/graph-dc/index.html'));
+res.sendFile(path.join(__dirname+'/graph-dc/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
