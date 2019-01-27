93e1857b5a641448a026ea104342a4d57a516fb4
iyerrk1811 <30715370+iyerrk1811@users.noreply.github.com>
2019-01-27 13:42:15 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 /*app.component.ts*/
 import { Component, OnInit } from '@angular/core';
-import * as CanvasJS from '../../node_modules/canvasjs-2.3/canvasjs.min';
+import * as CanvasJS from '../../node_modules/canvasjs/dist/canvasjs.min';
 // var CanvasJS = require('./canvasjs.min');
  
 @Component({
